fe791463eeb9e72981a8b692f1fda2ddc5049c66
NEKETA <molnei1996@gmail.com>
2023-02-12 02:25:19 +0300

    added zad2

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число а: ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число а: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число b: ");
 int b = Convert.ToInt32(Console.ReadLine());
---
+++ zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c)
+{
+    Console.WriteLine("max "+a);
+} else if (b>c)
+{
+    Console.WriteLine("max "+b);
+} else
+{
+    Console.WriteLine("max "+c);
+}
