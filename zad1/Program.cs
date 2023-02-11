7aa1b892a7b15fd2f713d9fa3949c4c79f386f9c
NEKETA <molnei1996@gmail.com>
2023-02-12 01:54:48 +0300

    added zad1

---
+++ zad1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("max "+a);
+} else if(b>a)
+{
+    Console.WriteLine("max "+b);
+} else
+{
+    Console.WriteLine("равны");
+}
